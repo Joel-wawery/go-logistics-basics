7cdb941022f72db7a4b17136d53bd4897c727248
Joel-wawery <163989746+Joel-wawery@users.noreply.github.com>
2026-02-02 09:03:35 +0300

    learning about composite types and their functions especially slices over arrays why should we use slices over arrays

--- main.go
+++ main.go
@@ -2,13 +2,21 @@ package main
 
 import "fmt"
 
-const maxShillingsGoal int = 20000000
+
 
 func main() {
-	//variable to store the amount of shillings each job pays
-	var eachPayingJob int = 1000
-	//variable to store the number of jobs needed to reach the goal
-	numberOfJobs := maxShillingsGoal / eachPayingJob
-	fmt.Println(numberOfJobs)
+	//code to create a slice where we are expecting 5 orders but it can grow or shrink
+	orders := make([]float64, 0 , 5)
+
+	//adding data to the slice and here the append checks capacity if its full it will allocate a bigger memory
+
+	orders = append(orders, 120.5 ,450.5 , 789.5)
+
+	fmt.Println("Orders: ", orders)
+	fmt.Println("Length: ", len(orders))
+	fmt.Println("Capacity: ", cap(orders))
+
+	firstTwoOrders := orders[:2]
+	fmt.Println("First two orders: ", firstTwoOrders)
 
 }
