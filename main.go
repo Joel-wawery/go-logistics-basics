cc2d13da162223cdbb17044e20e15b9539820363
Joel-wawery <163989746+Joel-wawery@users.noreply.github.com>
2026-02-03 09:24:37 +0300

    this is code to create structs which are the blueprint for any system we want to build and then we create a map as soon as we created the struct and the actual thing we were creating a blueprint for..we use a map to create a lookup table from the struct itself which maps to the actual data we made

--- main.go
+++ main.go
@@ -3,27 +3,42 @@ package main
 import "fmt"
 
 
+//code to create a struct for our trucking system(blueprint for our system)
+	type Truck struct {
+		ID  string
+		status  string
+		capacity  int
+		currentLoad  float64
+		location  string
+
+	}
+
 
 func main() {
-	//code to create a new map for a fleet of trucks i have for my system
-	//the key is the ID of the truck and the value is the current status of the truck
-	truckFleet := map[string]string{
-		"Truck1": "Available",
-		"Truck2": "In Transit",
-		"Truck3": "Under Maintenance",
-	}	
-	//code to add a fleet to the map
-	truckFleet["Truck4"] = "Available"
-	truckFleet["Truck5"] = "In Transit"
-	//code to update the status of the map
-	truckFleet["Truck2"] = "has finished delivery of the product available"
-	//code to delete a particular truck since its not in operation
-	delete(truckFleet, "Truck3")
-	//print the final map
-	fmt.Println(truckFleet)
-
-	//code to lopp throough the map and print the staus of each truck and inspect through range
-	for TruckId , status := range truckFleet {
-		fmt.Printf("Truck ID: %s, Status: %s\n",TruckId , status)
+	//creating a real truck and filling up its details 
+	truck1 := Truck{
+		ID : "T001",
+		status : "available",
+		capacity : 10000,
+		currentLoad : 0.0,
+		location : "Warehouse A",
 	}
+	fmt.Println(truck1)
+
+	fleet := make(map[Truck]Truck)
+	fleet[truck1] = Truck {
+		ID : "T002",
+		status : "in transit",
+		capacity : 15000,
+		currentLoad : 5000.0,
+		location : "On Route to City B",
+	}
+   	fmt.Println(fleet)
+
+	//looping through the fleet of trucks and printing their details
+	for key, value := range fleet {
+		fmt.Println("Truck Key:", key)
+		fmt.Println("Truck Value:", value)
+	}
+	
 }
