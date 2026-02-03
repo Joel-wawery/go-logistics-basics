07de3dd52cb379f6572feff2337deb3938cad6b7
Joel-wawery <163989746+Joel-wawery@users.noreply.github.com>
2026-02-03 08:14:41 +0300

    this is practicing how to write flexible slices that grow or shrink based on the data you have and practicing to write composite types which are variables that allow for multiple values at ago

--- main.go
+++ main.go
@@ -5,18 +5,17 @@ import "fmt"
 
 
 func main() {
-	//code to create a slice where we are expecting 5 orders but it can grow or shrink
-	orders := make([]float64, 0 , 5)
-
-	//adding data to the slice and here the append checks capacity if its full it will allocate a bigger memory
-
-	orders = append(orders, 120.5 ,450.5 , 789.5)
-
-	fmt.Println("Orders: ", orders)
-	fmt.Println("Length: ", len(orders))
-	fmt.Println("Capacity: ", cap(orders))
-
-	firstTwoOrders := orders[:2]
-	fmt.Println("First two orders: ", firstTwoOrders)
-
+	//code to prepare the list of the number of books we have
+	booksAvailable := make([]int ,0 , 9)
+	
+	//code to append the delivery times that have already been made
+	booksAvailable = append(booksAvailable, 5 , 13 , 16 , 17 , 19 , 145 ,13545)
+	//code to print out the deliveries made so far
+	fmt.Println("All deliveries made:", booksAvailable)
+	
+	//code to slice the list to show the first 2 deliveries made
+	booksAvailed := booksAvailable[:5]
+	
+	//code to print out the first two deliveries made
+	fmt.Println("all the main books availed:", booksAvailed)
 }
